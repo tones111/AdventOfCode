ca902e49f15358c9cb5a13730e0fa3e0ba402ab3
Paul Sbarra <Sbarra.Paul@gmail.com>
2015-12-23 13:20:31 -0600

    Day 1

---
+++ 01/main.rs
@@ -0,0 +1,21 @@
+use std::io::{self, Read};
+
+fn main() {
+
+   let mut basement = 0;
+   let mut floor = 0;
+
+   for (i, b) in io::stdin().bytes().enumerate() {
+      floor += match b.unwrap() as char {
+         '(' => 1,
+         ')' => -1,
+         _   => 0
+      };
+
+      if floor == -1 && basement == 0 {
+         basement = i+1;
+         println!("basement: {}", basement);
+      }
+   }
+   println!("floor: {}", floor);
+}
