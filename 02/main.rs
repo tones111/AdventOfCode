3fe5a643f0f733cf2f608c90fcce670aebec591e
Paul Sbarra <Sbarra.Paul@gmail.com>
2015-12-27 14:00:35 -0600

    Day 2: use fancier (but slower) calculations
    This change is more to demonstrate examples of how one can do
    interesting things with iterators.

--- 02/main.rs
+++ 02/main.rs
@@ -1,52 +1,35 @@
 use std::io;
+use std::ops::Mul;
 
 fn main() {
-    let mut area = 0;
-    let mut length = 0;
+    let mut total_area = 0;
+    let mut total_length = 0;
 
     let mut line = String::new();
     loop {
         line.clear();
         io::stdin().read_line(&mut line).unwrap();
+
         if line.len() == 0 {
                 break;
         }
-        let dims: Vec<_> = line.trim_right().split('x')
-            .map(|x| x.parse::<u32>().unwrap())
-            .collect();
-        let l = dims[0];
-        let w = dims[1];
-        let h = dims[2];
 
-        area += 2 * l * w + 2 * l * h + 2 * w * h + slack(l, w, h);
-        length += ribbon(l, w, h);
+        let mut dims: Vec<usize> = line.trim_right().split('x')
+            .map(|n| n.parse().unwrap()).collect();
+        dims.sort();
+
+        // Puzzle 1: calculate area of paper required
+        let faces = [dims[0]*dims[1], dims[0]*dims[2], dims[1]*dims[2]];
+        let area = faces.iter().fold(0, |acc, n| acc + 2 * n);
+        let min_face = faces.iter().fold(std::usize::MAX, |a, b| std::cmp::min(a, *b));
+        total_area += area + min_face;
+
+        // Puzzle 2: calculate length of ribbon required
+        let volume = dims.iter().fold(1, Mul::mul);
+        let smallest_per = dims.iter().take(2).collect::<Vec<&usize>>().iter()
+            .fold(0, |acc, n| acc + 2 * *n);
+        total_length += volume + smallest_per;
     }
-    println!("area: {}", area);
-    println!("length: {}", length);
-}
-
-fn min(a: u32, b: u32) -> u32 {
-    if a < b {
-        a
-    } else {
-        b
-    }
-}
-
-fn short_sides(l: u32, w: u32, h: u32) -> (u32, u32) {
-    if l < w {
-        (l, min(w, h))
-    } else {
-        (w, min(l, h))
-    }
-}
-
-fn slack(l: u32, w: u32, h: u32) -> u32 {
-    let (s1, s2) = short_sides(l, w, h);
-    s1 * s2
-}
-
-fn ribbon(l: u32, w: u32, h: u32) -> u32 {
-    let (s1, s2) = short_sides(l, w, h);
-    2 * s1 + 2 * s2 + l * w * h
+    println!("area: {}", total_area);
+    println!("length: {}", total_length);
 }
