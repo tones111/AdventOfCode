22adfcaa4369c0d1baf18953eb7d14e5e285e178
Paul Sbarra <Sbarra.Paul@gmail.com>
2016-10-29 22:47:50 -0500

    Day 12

---
+++ 12/src/main.rs
@@ -0,0 +1,73 @@
+use std::io::{self, Read};
+
+extern crate serde_json;
+
+use serde_json::Value;
+
+fn main() {
+
+    let mut buffer = String::new();
+    io::stdin().read_to_string(&mut buffer).unwrap();
+    let data: Value = serde_json::from_str(&buffer).unwrap();
+    println!("all: {}", sum(&data));
+    println!("no red: {}", sum_nored(&data));
+}
+
+fn sum(v: &Value) -> i64 {
+    match *v {
+        Value::I64(i) => i,
+        Value::U64(u) => u as i64,
+        Value::Array(ref a) => a.iter().map(|v| sum(v)).sum(),
+        Value::Object(ref m) => m.values().map(|v| sum(v)).sum(),
+
+        Value::Null => {
+            println!("Null");
+            0
+        }
+        Value::Bool(_) => {
+            println!("Bool");
+            0
+        }
+        Value::F64(_) => {
+            println!("F64");
+            0
+        }
+        Value::String(_) => 0,
+    }
+}
+
+fn sum_nored(v: &Value) -> i64 {
+    match *v {
+        Value::I64(i) => i,
+        Value::U64(u) => u as i64,
+        Value::Array(ref a) => a.iter().map(|v| sum_nored(v)).sum(),
+        Value::Object(ref m) => {
+            if m.values()
+                .filter(|v| {
+                    match **v {
+                        Value::String(ref s) if s == "red" => true,
+                        _ => false,
+                    }
+                })
+                .count() > 0 {
+                0
+            } else {
+                m.values().map(|v| sum_nored(v)).sum()
+            }
+        }
+
+        Value::Null => {
+            println!("Null");
+            0
+        }
+        Value::Bool(_) => {
+            println!("Bool");
+            0
+        }
+        Value::F64(_) => {
+            println!("F64");
+            0
+        }
+        Value::String(_) => 0,
+    }
+}
